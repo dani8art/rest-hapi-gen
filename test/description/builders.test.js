d815f8494ec9d6f5c4156b5a4636df2c2936dc92
Daniel Arteaga <dani8art.da@gmail.com>
2023-03-18 12:56:02 +0000

    feat(oas): pluralize oas path summaries
    Signed-off-by: Daniel Arteaga <dani8art.da@gmail.com>

--- test/description/builders.test.js
+++ test/description/builders.test.js
@@ -12,24 +12,24 @@ describe('Description Builders Tests', () => {
   it('#getResourceDescriptionBuilder should return the proper description', () => {
     const actualDecription = builders.getResourceDescriptionBuilder(collectionName);
 
-    expect(actualDecription).toBe('Gets an existing tests');
+    expect(actualDecription).toBe('Gets an existing test');
   });
 
   it('#createResourceDescriptionBuilder should return the proper description', () => {
     const actualDecription = builders.createResourceDescriptionBuilder(collectionName);
 
-    expect(actualDecription).toBe('Creates a new tests');
+    expect(actualDecription).toBe('Creates a new test');
   });
 
   it('#updateResourceDescriptionBuilder should return the proper description', () => {
     const actualDecription = builders.updateResourceDescriptionBuilder(collectionName);
 
-    expect(actualDecription).toBe('Updates an existing tests');
+    expect(actualDecription).toBe('Updates an existing test');
   });
 
   it('#deleteResourceDescriptionBuilder should return the proper description', () => {
     const actualDecription = builders.deleteResourceDescriptionBuilder(collectionName);
 
-    expect(actualDecription).toBe('Deletes an existing tests');
+    expect(actualDecription).toBe('Deletes an existing test');
   });
 });
